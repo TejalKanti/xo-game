6cf2560c21b5bf2ac031bb798e062026a34dafb2
TejalKanti <st10513267@myemeris.edu.za>
2026-08-25 13:12:39 +0200

    code: updated part 3 and part 4

--- App.tsx
+++ App.tsx
@@ -39,9 +39,7 @@ function PlayerScreen( { navigation }: any ){
 
      <Button 
         title= "Start Game"
-        onPress={() =>
-          navigation.navigate('Game', { player1: player1, player2: player2 })
-        }
+        onPress={() => navigation.navigate('Game', {player1: player1, player2: player2})}
         />
     </View>
   );
@@ -65,23 +63,149 @@ function GameScreen( { navigation, route }: any ){
   const [block9, setBlock9] = useState<number>(0);
 
   let block1Content: string;
-  if(block1 === 0){
-    block1Content = '';
-  } else if (block1 === 1){
-    block1Content = 'X';
-  } else {
-    block1Content = 'O';
+  switch (block1) {
+    case 0:
+      block1Content = '';
+      break;
+    case 1:
+      block1Content = 'X';
+      break;
+    case 2:
+      block1Content = 'O';
+      break;
+    default:
+      block1Content = '';
+  }
+
+  let block2Content: string;
+  switch (block2) {
+    case 0:
+      block2Content = '';
+      break;
+    case 1:
+      block2Content = 'X';
+      break;
+    case 2:
+      block2Content = 'O';
+      break;
+    default:
+      block2Content = '';
+  }
+
+  let block3Content: string;
+  switch (block3) {
+    case 0:
+      block3Content = '';
+      break;
+    case 1:
+      block3Content = 'X';
+      break;
+    case 2:
+      block3Content = 'O';
+      break;
+    default:
+      block3Content = '';
+  }
+
+  let block4Content: string;
+  switch (block4) {
+    case 0:
+      block4Content = '';
+      break;
+    case 1:
+      block4Content = 'X';
+      break;
+    case 2:
+      block4Content = 'O';
+      break;
+    default:
+      block4Content = '';
+  }
+
+  let block5Content: string;
+  switch (block5) {
+    case 0:
+      block5Content = '';
+      break;
+    case 1:
+      block5Content = 'X';
+      break;
+    case 2:
+      block5Content = 'O';
+      break;
+    default:
+      block5Content = '';
+  }
+
+  let block6Content: string;
+  switch (block6) {
+    case 0:
+      block6Content = '';
+      break;
+    case 1:
+      block6Content = 'X';
+      break;
+    case 2:
+      block6Content = 'O';
+      break;
+    default:
+      block6Content = '';
+  }
+
+  let block7Content: string;
+  switch (block7) {
+    case 0:
+      block7Content = '';
+      break;
+    case 1:
+      block7Content = 'X';
+      break;
+    case 2:
+      block7Content = 'O';
+      break;
+    default:
+      block7Content = '';
+  }
+
+  let block8Content: string;
+  switch (block8) {
+    case 0:
+      block8Content = '';
+      break;
+    case 1:
+      block8Content = 'X';
+      break;
+    case 2:
+      block8Content = 'O';
+      break;
+    default:
+      block8Content = '';
+  }
+
+  let block9Content: string;
+  switch (block9) {
+    case 0:
+      block9Content = '';
+      break;
+    case 1:
+      block9Content = 'X';
+      break;
+    case 2:
+      block9Content = 'O';
+      break;
+    default:
+      block9Content = '';
   }
 
   const handlePress = (index: number) => {
-    // index = 0 - that is block
+    console.log(`Cell ${index} pressed`);
     if (player1Turn){
-      setBlock1(1);
-    }  else {
+      setBlock1(1); 
+    } else {
       setBlock1(2);
     }
-    setPlayer1Turn(!player1Turn); // changed to the next person's turn
-  }; 
+    setPlayer1Turn(!player1Turn);
+  };
 
 
 return (
@@ -98,39 +222,39 @@ return (
           </TouchableHighlight>
 
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(1)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block2Content}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(2)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block3Content}</Text>
           </TouchableHighlight>
         </View>
 
         <View style={styles.row}>
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(3)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block4Content}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(4)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block5Content}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(5)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block6Content}</Text>
           </TouchableHighlight>
       </View>
 
         <View style={styles.row}>
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(6)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block7Content}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(7)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block8Content}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight style={styles.cell} onPress={() => handlePress(8)}>
-            <Text style={styles.cellText}></Text>
+            <Text style={styles.cellText}>{block9Content}</Text>
           </TouchableHighlight>
       </View>
 
